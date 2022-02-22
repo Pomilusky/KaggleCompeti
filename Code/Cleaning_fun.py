70b73ed77de50fa18d3900d50a22fda6c7e2fdc4
Pomilusky <marcnosas@hotmail.com>
2022-02-22 19:03:09 +0100

    Update

--- Code/Cleaning_fun.py
+++ Code/Cleaning_fun.py
@@ -3,6 +3,7 @@ from geopy.geocoders import Nominatim
 import reverse_geocoder 
 from langdetect import detect, DetectorFactory
 import re
+import numpy as np
 DetectorFactory.seed = 3
 
 ###############################################################################################
@@ -24,8 +25,12 @@ def num_NaNs(df, n=0):
     return NaNs_dict
 
 ###############################################################################################
-not_rel = ['id', 'scrape_id', 'last_scraped', 'name','picture_url', 'host_id', 'host_url','host_location', 'license',
-'host_name','instant_bookable','neighborhood_overview']
+not_rel =['id', 'scrape_id', 'last_scraped', 'name','picture_url', 'host_id', 'host_url','host_location', 'license',
+'host_name','instant_bookable','neighborhood_overview','neighbourhood_group_cleansed','calendar_updated','host_thumbnail_url','availability_30',
+'availability_60', 'availability_90','calendar_last_scraped','listing_url','property_type','bathrooms','host_picture_url','host_listings_count',
+'neighbourhood','beds','minimum_minimum_nights', 'maximum_minimum_nights', 'minimum_maximum_nights', 'maximum_maximum_nights','minimum_nights_avg_ntm', 
+'maximum_nights_avg_ntm','number_of_reviews_ltm','number_of_reviews_l30d','first_review', 'last_review','review_scores_value','calculated_host_listings_count',
+'calculated_host_listings_count_entire_homes','calculated_host_listings_count_private_rooms','calculated_host_listings_count_shared_rooms']
 def Clean_columns(df, colms = not_rel, inpl = False):
     return df.drop(columns = colms, inplace=inpl)
 
@@ -64,4 +69,64 @@ def bath_clean(s):
         if re.findall('\d',str(s)): return re.findall('\d',s)[0]
         else: return 0
 
+###############################################################################################
+
+def lang_clean(des, ha):
+    if des == ha and des == 'en': # everything in english
+        return 0
+    elif des != ha and des == 'en':
+        return 1
+    elif des != ha and ha == 'en':
+        return 2
+    else:
+        return 3
+
+###############################################################################################
+
+def cl_host_response_time(x):
+    if x == 'within an hour': return 0
+    if x in ['within a few hours', 'within a day']: return 1
+    if x == np.nan: return np.nan
+    else: return 2
+
+###############################################################################################
+
+def cl_host_verifications(x):
+    x.replace("'",'').replace('[','').replace(']','').replace(' ','').lower().split(',') # let's turn the input into a list
+    if len(x) == 2: return 0
+    elif len(x)>2 and 'reviews' in x: return 2
+    else: return 1
+
+###############################################################################################
+def vectorize(df):
+    	
+    df.host_is_superhost = df.host_is_superhost.apply(lambda x: 1 if x=='t' else 0)
+    #df.instant_bookable = df.instant_bookable.apply(lambda x: 1 if x=='t' else 0)
+    df.host_has_profile_pic=df.host_has_profile_pic.apply(lambda x: 1 if x=='t' else 0)
+    df.has_availability = df.has_availability.apply(lambda x: 0 if x=='t' else 1)
+    df.host_identity_verified =	df.host_identity_verified.apply(lambda x: 0 if x=='t' else 1)
+    
+    df.host_acceptance_rate = df.host_acceptance_rate.apply(lambda x: np.nan if type(x) == float else pd.to_numeric(x[:-1]))
+    df.host_response_rate = df.host_response_rate.apply(lambda x: np.nan if type(x) == float else pd.to_numeric(x[:-1]))
+    df.host_since = df.host_since.apply(lambda x: np.nan if x==np.nan else 2022-int(re.findall('\d{4}',x)[0]))
+    #df.bathrooms_text = df.bathrooms_text(bath_clean)
+    df['host_response_time'] = df.host_response_time.apply(cl_host_response_time)
+    df['host_verifications'] = df.host_verifications.apply(cl_host_verifications)
+    
+    
+    
+    df['lang_des']=detect_lan(df, 'description')
+    df['lang_host']=detect_lan(df, 'host_about')
+    df['lang_var'] =df.apply(lambda x: lang_clean(x.lang_des,x.lang_host), axis=1)
+    df.drop(columns=['host_about','description','lang_des','lang_host'], inplace=True)
+
+    df[[i.lower().replace(' ','_') for i in df.room_type.unique()]] = pd.get_dummies(df.room_type)
+    df.drop(columns=['room_type'], inplace=True)
+    
+    return df
+
+
+###############################################################################################
+
+###############################################################################################
 ###############################################################################################
---
+++ Code/scrap.py
@@ -0,0 +1,30 @@
+import pandas as pd
+from selenium import webdriver
+import numpy as np
+
+# I am going to scrap the price of the airbnb in descember last year, in order to try and guess the price from there,
+df = pd.read_csv('../Data/train.csv') # let's import the data frame
+
+# let's create the function to scrap each url,
+def scrap_price(l_url):
+    try:
+        url = l_url+'?_set_bev_on_new_domain=1645460202_MDJlNWU5MDVhMmYw&source_impression_id=p3_1645461268_jxnf6JF4LiYEjqYh&check_in=2022-12-21&guests=1&adults=1&check_out=2022-12-22'
+        options = webdriver.ChromeOptions()
+        options.add_argument("headless")
+        options.add_argument("no_sandbox")
+        driver = webdriver.Chrome(executable_path='/home/pomilusky/Escriptori/Ironhack/chromedriver',chrome_options=options)
+        driver.get(url)
+        driver.implicitly_wait(5)
+        driver.find_element_by_xpath('/html/body/div[5]/div/div/div[1]/div/div[3]/section/div[2]/div[2]/button').click()
+        a =driver.find_element_by_xpath('//*[@id="site-content"]/div/div[1]/div[3]/div/div[2]/div/div/div[1]/div/div/div/div/div/div/div/div[1]/div[1]/div/div/div/span[1]').text
+        driver.quit()
+        return int(a[1:])
+    except: 
+        driver.quit()
+        return np.nan # If we can' acces the url we are going to give it a np.nan value
+
+df['price_now'] = df.listing_url.apply(scrap_price)
+df.to_csv('../Data/ambPriceNow.csv',mode='w', index=False, header=True)
+# Let's do the same with the test,
+df_test = pd.read_csv('../Data/test.csv')
+df_test.to_csv('../Data/TestAmbPriceNow.csv', mode='w', index=False, header=True)
